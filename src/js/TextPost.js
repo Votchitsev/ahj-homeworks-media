25eb4dc9f5d59582925779d41e1f9ff966da83ec
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-09-19 21:12:53 +0200

    Create work env

---
+++ src/js/TextPost.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-useless-constructor */
+/**
+ * Текстовый пост
+ */
+class TextPost {
+  /**
+   * @param {*} template HTML element
+   * @param {String} postText текст поста
+   */
+  // eslint-disable-next-line no-empty-function
+  constructor(template, postText) {
+
+  }
+
+  /**
+   * Возвращает готовый пост для отображения
+   */
+  create() {
+
+  }
+
+  /**
+   * Получает геолокацию
+   */
+  getGeolocation() {
+
+  }
+  /**
+   * Получает текущее время
+   */
+
+  getCurrentTime() {
+
+  }
+}
+
+export default TextPost;

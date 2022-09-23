9e5cd0b2148c451e5aadb2ad4fc62e66f7178906
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-09-23 22:14:03 +0200

    test created

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,1 +1,36 @@
-// write tests here;
+/**
+ * @jest-environment jsdom
+ */
+
+describe('Test coord', () => {
+  const domEnvironment = document.createElement('form');
+  domEnvironment.classList.add('error-popup');
+  const input = document.createElement('input');
+  input.setAttribute('type', 'text');
+  input.setAttribute('pattern', '\\[?\\d+.\\d+,\\s?.?\\d+.\\d+\\]?');
+  domEnvironment.append(input);
+
+  test('check 51.50851, -0.12572', () => {
+    input.value = '[51.50851, 0.12572]';
+    const result = input.checkValidity();
+    expect(result).toBeTruthy();
+  });
+
+  test('check 51.50851,-0.12572', () => {
+    input.value = '51.50851,-0.12572';
+    const result = input.checkValidity();
+    expect(result).toBeTruthy();
+  });
+
+  test('check [51.50851, -0.12572]', () => {
+    input.value = '[51.50851, -0.12572]';
+    const result = input.checkValidity();
+    expect(result).toBeTruthy();
+  });
+
+  test('check 123', () => {
+    input.value = '123';
+    const result = input.checkValidity();
+    expect(result).not.toBeTruthy();
+  });
+});
